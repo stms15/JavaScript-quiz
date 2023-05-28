fd67993588cedab3fc051cbc1a7c92ab2436849c
Sarah S <stms15@mun.ca>
2023-05-28 13:18:56 -0230

    added button functions on leaderboard page

--- assets/js/script-leaderboard.js
+++ assets/js/script-leaderboard.js
@@ -1,7 +1,7 @@
 var leaderboardList = document.getElementById('leaderboard');
+var clearBttn = document.getElementById('clear-board');
 
 leaders = JSON.parse(localStorage.getItem("leadersList"));
-console.log(leaders);
 
 for (let i=0; i<leaderboardList.children.length; i++){
     leaderboardList.removeChild(leaderboardList.children[i]);
@@ -11,3 +11,8 @@ for (let j=0; j<leaders.length; j++){
     newLiEl.textContent = leaders[j].user + " - " + leaders[j].score;
     leaderboardList.appendChild(newLiEl);
 }
+
+clearBttn.addEventListener("click", function() {
+    leaderboardList.innerHTML = "";
+    localStorage.setItem("leadersList", null);
+})
